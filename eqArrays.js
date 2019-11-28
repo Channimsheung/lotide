59385dc8dda88acb3b2873853a37a0be03069a65
Channimsheung <msj@live.hk>
2019-11-28 00:24:51 +0000

    eqarrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,36 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`This is true`);
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    //console.log(`The length are unequal`);
+    return false;
   } else {
-    console.log(`This is false`);
-  }
-};
-
-const eqArrays = function(arrOne, arrTwo) {
-  for (let i = 0; i < arrOne.length; i++) {
-    if (arrOne[i] !== arrTwo[i]) {
-      return false;
+    // console.log(`The length are equal`);
+    for (let i = 0; i < Object.keys(object1).length; i++) {
+      if (
+        Array.isArray(Object.values(object1)[i]) &&
+        Array.isArray(Object.values(object2)[i])
+      ) {
+        return eqArrays(Object.values(object1)[i], Object.values(object2)[i]);
+      }
+      //console.log(Object.keys(object1)[i])
+      //console.log(Object.keys(object2)[i])
+      if (Object.values(object1)[i] !== Object.values(object2)[i]) {
+        //console.log('The arrays are not equal)
+        return false;
+      }
     }
   }
   return true;
 };
-eqArrays([1, 2, 3], [1, 2, 8]);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arrOne.length; i++) {
+      if (arrOne[i] !== arrTwo[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
