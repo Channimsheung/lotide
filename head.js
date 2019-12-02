60110ade3687203369e92b51fb7801a3d1f80816
Channimsheung <msj@live.hk>
2019-12-02 02:35:46 +0000

    changedinWk2

--- head.js
+++ head.js
@@ -1,15 +1,9 @@
-const assertEqual = function(newStr, expected) {
-  if (newStr !== expected) {
-    console.log(`This is false`);
-  } else {
-    console.log(`This is true`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const head = function(arrayOfNum) {
   let newStr = "";
   newStr += arrayOfNum[0];
   return Number(newStr);
 };
-head([6, 7, 8]);
-assertEqual(head([6, 7, 8]), 6);
+
+module.exports = head;
